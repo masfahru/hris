ba8ecabf043c1f0fa686ebc16e222e5776231251
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-10-08 07:40:23 +0700

    update pagination schema: use type numeric and enums, countOffset should not return negative number

--- src/common/typebox/schemas/pagination.ts
+++ src/common/typebox/schemas/pagination.ts
@@ -1,16 +1,21 @@
 import { Static } from "@sinclair/typebox";
 import { t } from "elysia";
 
+enum Order {
+	// biome-ignore lint/style/useNamingConvention: common name
+	ASC = "ASC",
+	// biome-ignore lint/style/useNamingConvention: common name
+	DESC = "DESC",
+	// biome-ignore lint/style/useNamingConvention: common name
+	asc = "asc",
+	// biome-ignore lint/style/useNamingConvention: common name
+	desc = "desc",
+}
+
 export const paginationSchema = t.Object({
-	page: t.Number({
-		minimum: 1,
-	}),
-	limit: t.Number({
-		minimum: 1,
-	}),
-	order: t.String({
-		enum: ["ASC", "DESC", "asc", "desc"],
-	}),
+	page: t.Numeric(),
+	limit: t.Numeric(),
+	order: t.Enum(Order),
 	orderBy: t.String({
 		minLength: 1,
 	}),
@@ -19,5 +24,9 @@ export const paginationSchema = t.Object({
 export type PaginationSchema = Static<typeof paginationSchema>;
 
 export const countOffset = (page: number, limit: number) => {
+	// offset should not return negative number
+	if (page < 1 || limit < 1) {
+		return 0;
+	}
 	return (page - 1) * limit;
 };
