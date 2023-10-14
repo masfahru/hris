a4669153d439851ca6c7d2922ec4fc5aee9765e1
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-10-14 08:47:40 +0700

    Restart bun child process if killed

--- spawn.ts
+++ spawn.ts
@@ -1,12 +1,22 @@
 import os from "node:os";
 import Bun from "bun";
 
+const command = ["bun", "src/index.ts"]
+
+const options = {
+	stdio: ["inherit", "inherit", "inherit"],
+	env: { ...process.env },
+  onExit: (proc) => {
+    if (proc.killed) {
+      Bun.spawn(command, options);
+    } 
+  }
+};
+
+
 // @ts-ignore
 const numCpus = os.availableParallelism();
 
 for (let i = 0; i < numCpus; i++) {
-	Bun.spawn(["bun", "src/index.ts"], {
-		stdio: ["inherit", "inherit", "inherit"],
-		env: { ...process.env },
-	});
+	Bun.spawn(command, options);
 }
